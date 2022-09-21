133e2f4deeb2804b823f159672c48fbc28e6709f
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2022-09-21 09:50:48 -0300

    Ejercicio terminado

--- src/Library/AllInOnePrinter.cs
+++ src/Library/AllInOnePrinter.cs
@@ -29,3 +29,4 @@ namespace Full_GRASP_And_SOLID.Library
         }
     }
 }
+/*Esta clase es incorrecta ya que viola lo que el patrón de polimorfismo dice que no hay que hacer. Esto se debe a que el código que varía según su destino debería de estar en diferentes clases, y deberíamos utilizar una operación polimórfica para solucionarlo. En este caso, utilizaré un tipo IPrinter y dos clases FilePrinter y ConsolePrinten que lo implementen.*/
